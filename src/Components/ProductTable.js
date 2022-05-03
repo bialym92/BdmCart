1bde8691a2d6f0017f9389db866c48cad9a8cb4f
Maciej Białecki <=bialy.m92@gmail.com>
2022-05-03 17:29:03 +0200

    added selfexpanding table

--- src/Components/ProductTable.js
+++ src/Components/ProductTable.js
@@ -1,26 +1,59 @@
 import React, { Fragment } from "react";
 import BdmProductButton from "./BdmProductButton";
 
+const queriedPlants = [
+  //DUMMIE__DATA
+  {
+    id: 1,
+    polishName: "brzoza",
+    latinName: "Betula",
+    bdmQuantity: 11,
+    bdmLocalization: "A1",
+  },
+  {
+    id: 2,
+    polishName: "świerk",
+    latinName: "Pinus",
+    bdmQuantity: 200,
+    bdmLocalization: "B2",
+  },
+  {
+    id: 3,
+    polishName: "cis",
+    latinName: "Taxus",
+    bdmQuantity: 45,
+    bdmLocalization: "C3",
+  },
+];
+
 const ProductTable = () => {
   return (
     <Fragment>
       <table>
         <tr>
-          <th>Company</th>
-          <th>Contact</th>
-          <th>Country</th>
+          <th>Nazwa pl.</th>
+          <th>Nazwa lac.</th>
+          <th>Dostępna ilość</th>
+          <th>Lokalizacja</th>
         </tr>
-        <tr>
-          <td>Alfreds Futterkiste</td>
-          <td>Maria Anders</td>
-          <td><BdmProductButton></BdmProductButton></td>
-        </tr>
-        <tr>
-          <td>Centro comercial Moctezuma</td>
-          <td>Francisco Chang</td>
-          <td><BdmProductButton></BdmProductButton></td>
-        </tr>
+        {queriedPlants.map((item)=>
+               { return(
+                <tr key={item.id}>
+                <td>{item.polishName}</td>
+                <td>{item.latinName}</td>
+                <td>{item.bdmQuantity}</td>
+                <td>{item.bdmLocalization}</td>
+                <td><BdmProductButton /></td>
+
+              </tr>)
+                
+                })}
+
       </table>
+
+
+
+
     </Fragment>
   );
 };
