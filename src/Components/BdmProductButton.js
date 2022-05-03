1de8d47dc53c9457bbb4e75bf5674dfdd64a8886
Maciej Białecki <=bialy.m92@gmail.com>
2022-05-03 16:34:01 +0200

    beggining settup 2

---
+++ src/Components/BdmProductButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const BdmProductButton = () => {
+  return (
+    <button>+</button>
+  );
+}
+
+export default BdmProductButton;
--- src/Components/ProductTable.js
+++ src/Components/ProductTable.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { Fragment } from "react";
+import BdmProductButton from "./BdmProductButton";
 
 const ProductTable = () => {
   return (
-    <div>
-      Goodbye
-    </div>
+    <Fragment>
+      <table>
+        <tr>
+          <th>Company</th>
+          <th>Contact</th>
+          <th>Country</th>
+        </tr>
+        <tr>
+          <td>Alfreds Futterkiste</td>
+          <td>Maria Anders</td>
+          <td><BdmProductButton></BdmProductButton></td>
+        </tr>
+        <tr>
+          <td>Centro comercial Moctezuma</td>
+          <td>Francisco Chang</td>
+          <td><BdmProductButton></BdmProductButton></td>
+        </tr>
+      </table>
+    </Fragment>
   );
-}
+};
 
 export default ProductTable;
