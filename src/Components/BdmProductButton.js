d52705d0c13052c3cb21ccd1e869d964ba37218e
Maciej Białecki <=bialy.m92@gmail.com>
2022-05-04 11:09:29 +0200

    b. setup 4

--- src/Components/BdmProductButton.js
+++ src/Components/BdmProductButton.js
@@ -1,13 +1,49 @@
 import React from 'react';
 
-
+const queriedPlants = [
+  //DUMMIE__DATA
+  {
+    id: 1,
+    polishName: "brzoza",
+    latinName: "Betula",
+    bdmQuantity: 11,
+    bdmLocalization: "A1",
+  },
+  {
+    id: 2,
+    polishName: "świerk",
+    latinName: "Pinus",
+    bdmQuantity: 200,
+    bdmLocalization: "B2",
+  },
+  {
+    id: 3,
+    polishName: "cis",
+    latinName: "Taxus",
+    bdmQuantity: 45,
+    bdmLocalization: "C3",
+  },
+];
 
   // czy rzeczywiście kazdy button dostaje id ktore przydziela mu BdmProductTable
 
 const BdmProductButton = (props) => {
 
   const bdmAddProduct = () => {
-    console.log(props.id)
+    console.log(props.id);
+    // const clickedProduct = queriedPlants.filter
+
+    function separateClickedProduct(arr, criteria) {
+      return arr.filter(function(obj) {
+        return Object.keys(criteria).every(function(c) {
+          return obj[c] == criteria[c];
+        });
+      });
+    }
+
+    console.log(separateClickedProduct(queriedPlants, { id: props.id}));
+
+
   };
 
   return (
